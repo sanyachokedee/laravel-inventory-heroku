e1f811d70baf2f6dd6e503ea6d66e486d5903f52
Nutthakit Anansuksakul <ing118199@gmail.com>
2021-11-09 18:07:01 +0700

    update User.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\user;  //ใช้ model ชื่อ user
+use App\Models\User;  //ใช้ model ชื่อ User
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
